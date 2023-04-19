575c9adeeeb401eb60f8702dbed22f6013db2994
Paul <paul.fleming@nrel.gov>
2023-04-19 16:15:15 -0600

    init and version

---
+++ flasc_pl/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for FLORIS SCADA Analysis repository."""
+
+__author__ = """Bart Doekemeijer"""
+__email__ = 'bart.doekemeijer@nrel.gov'
+__version__ = '1.0'
+
+from pathlib import Path
+
